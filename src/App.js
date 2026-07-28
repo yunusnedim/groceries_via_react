af27605f3b2f4293481f4bff2ab572a2344b9d94
yunusnedim <yunus.mehel@volvocars.com>
2026-07-28 14:26:45 +0200

    somewhat working game

--- src/App.js
+++ src/App.js
@@ -1,3 +1,149 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React, {useState} from 'react';
+
+function Square(props) {
+    return (
+        <button className='square' onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+function Information(props) {
+    if (props.winner) {
+        return <h1>Game Over. Winner: {props.winner}</h1>;
+    } else if (props.xturn) {
+        return <h1>Next turn: X</h1>;
+    } else {
+        return <h1>Next turn: O</h1>;
+    }
+}
+
+function Winner(props) {
+    if (props[0] === props[1] && props[1] === props[2]) {
+        return props[0];
+    } else if (props[3] === props[4] && props[4] === props[5]) {
+        return props[3];
+    } else if (props[6] === props[7] && props[7] === props[8]) {
+        return props[6];
+    } else if (props[0] === props[3] && props[3] === props[6]) {
+        return props[0];
+    } else if (props[1] === props[4] && props[4] === props[7]) {
+        return props[1];
+    } else if (props[2] === props[5] && props[5] === props[8]) {
+        return props[2];
+    } else if (props[0] === props[4] && props[4] === props[8]) {
+        return props[0];
+    } else if (props[2] === props[4] && props[4] === props[6]) {
+        return props[2];
+    } else {
+        return null;
+    }
+}
+
+function ResetBoard(props) {
+    return <button onClick={props.onClick}>Reset Board</button>;
+}
+
+function turnsLeft(props) {
+    return (
+        props.length -
+        props.filter((square) => square === 'X' || square === 'O').length
+    );
+}
+
+function clickHandler(squares, i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'O';
+    setSquares(nextSquares);
+}
+
+export default function Board() {
+    function clickHandler(squares, i) {
+        const nextSquares = squares.slice();
+        if (turnsLeft(squares) == 0) {
+            return;
+        }
+        nextSquares[i] = xturn ? 'X' : 'O';
+
+        setSquares(nextSquares);
+        setXturn(!xturn);
+    }
+
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xturn, setXturn] = useState(true);
+
+    const winner = Winner(squares);
+
+    return (
+        <>
+            <Information winner={winner} xturn={xturn} />
+
+            <div className='board-row'>
+                <Square
+                    value={squares[0]}
+                    onClick={() => {
+                        clickHandler(squares, 0);
+                    }}
+                />
+                <Square
+                    value={squares[1]}
+                    onClick={() => {
+                        clickHandler(squares, 1);
+                    }}
+                />
+                <Square
+                    value={squares[2]}
+                    onClick={() => {
+                        clickHandler(squares, 2);
+                    }}
+                />
+            </div>
+            <div className='board-row'>
+                <Square
+                    value={squares[3]}
+                    onClick={() => {
+                        clickHandler(squares, 3);
+                    }}
+                />
+                <Square
+                    value={squares[4]}
+                    onClick={() => {
+                        clickHandler(squares, 4);
+                    }}
+                />
+                <Square
+                    value={squares[5]}
+                    onClick={() => {
+                        clickHandler(squares, 5);
+                    }}
+                />
+            </div>
+            <div className='board-row'>
+                <Square
+                    value={squares[6]}
+                    onClick={() => {
+                        clickHandler(squares, 6);
+                    }}
+                />
+                <Square
+                    value={squares[7]}
+                    onClick={() => {
+                        clickHandler(squares, 7);
+                    }}
+                />
+                <Square
+                    value={squares[8]}
+                    onClick={() => {
+                        clickHandler(squares, 8);
+                    }}
+                />
+            </div>
+            <ResetBoard
+                onClick={() => {
+                    setSquares(Array(9).fill(null));
+                    setXturn(true);
+                }}
+            />
+        </>
+    );
 }
