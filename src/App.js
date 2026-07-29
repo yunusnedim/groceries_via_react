20b8b8e47f6b5fce4d55908402b6eeb3bd575f9e
yunusnedim <yunus.mehel@volvocars.com>
2026-07-29 10:47:14 +0200

    some static, skeleton UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
 import React, {useState} from 'react';
 
-export default function Board() {
+function SearchBar() {
+    return <h1>SearchBar</h1>;
+}
+
+function ProductTable(props) {
     return (
-        <button className='square' onClick={props.onClick}>
-            {props.value}
-        </button>
+        <div>
+            <div>
+                <ProductCategoryRow />
+                <ProductRow />
+                <ProductRow />
+            </div>
+
+            <div>
+                <ProductCategoryRow />
+                <ProductRow />
+                <ProductRow />
+            </div>
+        </div>
     );
 }
+
+function ProductCategoryRow() {
+    return <h2>ProductCategoryRow</h2>;
+}
+
+function ProductRow(props) {
+    return <h3>ProductRow</h3>;
+    // return (
+    //     <div>
+    //         {props.product.name}
+    //         {props.product.isInStock ? null : (
+    //             <span style={{color: 'red'}}> (Out of Stock)</span>
+    //         )}
+    //         {props.product.price.toFixed(2)}
+    //     </div>
+    // );
+}
+
+function FilterableProductTable(props) {
+    return (
+        <div>
+            <SearchBar />
+            <ProductTable products={props.products} />
+        </div>
+    );
+}
+
+export default function App() {
+    return <FilterableProductTable products={PRODUCTS} />;
+}
+
+const PRODUCTS = [
+    {category: 'Fruits', price: '$1', stocked: true, name: 'Apple'},
+    {category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit'},
+    {category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit'},
+    {category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach'},
+    {category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin'},
+    {category: 'Vegetables', price: '$1', stocked: true, name: 'Peas'},
+];
